b3fea59ce6d8a1d38836294f3cfee657745ca5b9
Pedro Henrique <pedrohenrique170496@gmail.com>
2022-11-19 16:17:35 -0300

    new file:   src/views/cosplay.js 	new file:   src/views/kpop.js 	modified:   src/views/widgets/WidgetsDropdown.js

--- src/views/widgets/WidgetsDropdown.js
+++ src/views/widgets/WidgetsDropdown.js
@@ -12,7 +12,7 @@ import {
 import CIcon from '@coreui/icons-react'
 import ChartLineSimple from '../charts/ChartLineSimple'
 import ChartBarSimple from '../charts/ChartBarSimple'
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 const WidgetsDropdown = () => {
 
@@ -20,106 +20,109 @@ const WidgetsDropdown = () => {
   const handleClick = () => {
     history.push('/wall')
   }
-  
+
   // render
   return (
     <CRow>
       <CCol sm="6" lg="3">
-      <CButton onClick={handleClick}  color='primary'>
-              <CWidgetDropdown        
-          color="gradient-primary"
-          header="COSPLAY"
-          text="Fantasias"
-          footerSlot={
-            <ChartLineSimple
-              pointed
-              className="c-chart-wrapper mt-3 mx-3"
-              style={{height: '70px'}}
-              dataPoints={[65, 59, 84, 84, 51, 55, 40]}
-              pointHoverBackgroundColor="primary"
-              label="Members"
-              labels="months"
-            />
-          }
-        >
-          <CDropdown>
-            <CDropdownToggle color="transparent">
-              <CIcon name="cil-settings"/>
-            </CDropdownToggle>
-            <CDropdownMenu className="pt-0" placement="bottom-end">
-              <CDropdownItem>Action</CDropdownItem>
-              <CDropdownItem>Another action</CDropdownItem>
-              <CDropdownItem>Something else here...</CDropdownItem>
-              <CDropdownItem disabled>Disabled action</CDropdownItem>
-            </CDropdownMenu>
-          </CDropdown>
-        </CWidgetDropdown>
-        </CButton>
+    
+          <CWidgetDropdown
+            color="gradient-primary"
+            header="COSPLAY"
+            text="Fantasias"
+			onClick={handleClick}
+            footerSlot={
+              <ChartLineSimple
+                pointed
+                className="c-chart-wrapper mt-3 mx-3"
+                style={{ height: '70px' }}
+                dataPoints={[65, 59, 84, 84, 51, 55, 40]}
+                pointHoverBackgroundColor="primary"
+                /* label="Members"
+                labels="months" */
+              />
+            }
+          >
+            {/* <CDropdown>
+              <CDropdownToggle color="transparent">
+                <CIcon name="cil-settings" />
+              </CDropdownToggle>
+              <CDropdownMenu className="pt-0" placement="bottom-end">
+                <CDropdownItem>Action</CDropdownItem>
+                <CDropdownItem>Another action</CDropdownItem>
+                <CDropdownItem>Something else here...</CDropdownItem>
+                <CDropdownItem disabled>Disabled action</CDropdownItem>
+              </CDropdownMenu>
+            </CDropdown> */}
+          </CWidgetDropdown>
+       
       </CCol>
 
       <CCol sm="6" lg="3">
-        <CButton onClick={handleClick}  color='primary'>
-        <CWidgetDropdown        
-          color="gradient-info"
-          header="K-POP"
-          text="Members online"
-          footerSlot={
-            <ChartLineSimple
-              pointed
-              className="mt-3 mx-3"
-              style={{height: '70px'}}
-              dataPoints={[1, 18, 9, 17, 34, 22, 11]}
-              pointHoverBackgroundColor="info"
-              options={{ elements: { line: { tension: 0.00001 }}}}
-              label="Members"
-              labels="months"
-            />
-          }
-        >
-          <CDropdown>
-            <CDropdownToggle caret={false} color="transparent">
-              <CIcon name="cil-location-pin"/>
-            </CDropdownToggle>
-            <CDropdownMenu className="pt-0" placement="bottom-end">
-              <CDropdownItem>Action</CDropdownItem>
-              <CDropdownItem>Another action</CDropdownItem>
-              <CDropdownItem>Something else here...</CDropdownItem>
-              <CDropdownItem disabled>Disabled action</CDropdownItem>
-            </CDropdownMenu>
-          </CDropdown>
-        </CWidgetDropdown>
-        </CButton>
-      </CCol>      
+        
+          <CWidgetDropdown
+            color="gradient-info"
+            header="K-POP"
+            text="Musicas "
+			onClick={handleClick}
+            footerSlot={
+              <ChartLineSimple
+                pointed
+                className="mt-3 mx-3"
+                style={{ height: '70px' }}
+                dataPoints={[1, 18, 9, 17, 34, 22, 11]}
+                pointHoverBackgroundColor="info"
+                options={{ elements: { line: { tension: 0.00001 } } }}
+                label="Members"
+                labels="months"
+              />
+            }
+          >
+            {/* <CDropdown>
+              <CDropdownToggle caret={false} color="transparent">
+                <CIcon name="cil-location-pin" />
+              </CDropdownToggle>
+              <CDropdownMenu className="pt-0" placement="bottom-end">
+                <CDropdownItem>Action</CDropdownItem>
+                <CDropdownItem>Another action</CDropdownItem>
+                <CDropdownItem>Something else here...</CDropdownItem>
+                <CDropdownItem disabled>Disabled action</CDropdownItem>
+              </CDropdownMenu>
+            </CDropdown> */}
+          </CWidgetDropdown>
+       
+      </CCol>
 
       <CCol sm="6" lg="3">
-        <CButton onClick={handleClick}  color='danger'>
-        <CWidgetDropdown
-          color="gradient-danger"
-          header="9.823"
-          text="Members online"
-          footerSlot={
-            <ChartBarSimple
-              className="mt-3 mx-3"
-              style={{height: '70px'}}
-              backgroundColor="rgb(250, 152, 152)"
-              label="Members"
-              labels="months"
-            />
-          }
-        >
-          <CDropdown>
-            <CDropdownToggle caret className="text-white" color="transparent">
-              <CIcon name="cil-settings"/>
-            </CDropdownToggle>
-            <CDropdownMenu className="pt-0" placement="bottom-end">
-              <CDropdownItem>Action</CDropdownItem>
-              <CDropdownItem>Another action</CDropdownItem>
-              <CDropdownItem>Something else here...</CDropdownItem>
-              <CDropdownItem disabled>Disabled action</CDropdownItem>
-            </CDropdownMenu>
-          </CDropdown>
-        </CWidgetDropdown>
-        </CButton>
+      
+          <CWidgetDropdown
+            color="gradient-danger"
+            header="LOL"
+            text="Gamer Players"
+			onClick={handleClick}
+            footerSlot={
+              <ChartBarSimple
+                className="mt-3 mx-3"
+                style={{ height: '70px' }}
+                backgroundColor="rgb(250, 152, 152)"
+               /*  label="Members"
+                labels="months" */
+              />
+            }
+          >
+            {/* <CDropdown>
+              <CDropdownToggle caret className="text-white" color="transparent">
+                <CIcon name="cil-settings" />
+              </CDropdownToggle>
+              <CDropdownMenu className="pt-0" placement="bottom-end">
+                <CDropdownItem>Action</CDropdownItem>
+                <CDropdownItem>Another action</CDropdownItem>
+                <CDropdownItem>Something else here...</CDropdownItem>
+                <CDropdownItem disabled>Disabled action</CDropdownItem>
+              </CDropdownMenu>
+            </CDropdown> */}
+          </CWidgetDropdown>
+      
       </CCol>
     </CRow>
   )
